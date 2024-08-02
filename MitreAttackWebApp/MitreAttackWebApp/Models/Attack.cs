c2010508132c86e2c920ff8b7fd34e6c68eb3547
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2024-08-02 15:40:51 +0300

    Add files via upload

---
+++ MitreAttackWebApp/MitreAttackWebApp/Models/Attack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MitreAttackWebApp.Models
+{
+    public class Attack
+    {
+        [Key]
+        public int AtackID { get; set; }
+        public string AttackCode { get; set; }
+        public string AttackResult { get; set; }
+        public DateTime AttackDate { get; set; }
+        public TimeSpan AttackTime { get; set; }
+    }
+}
---
+++ MitreAttackWebApp/MitreAttackWebApp/Models/AttackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MitreAttackWebApp.Models
+{
+    public class AttackDbContext : DbContext
+    {
+        public AttackDbContext() : base("name=MitreAttackDB")
+        {
+        }
+
+        public DbSet<Attack> Attacks { get; set; }
+    }
+}
