a7366b4fd1581df04f58d7e145d5d9832d795ac9
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2024-08-02 15:35:26 +0300

    MITRE ATT&CK agent software application added.

---
+++ MitreAttack/MitreAttack/Test.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MitreAttack
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblT1518 = new System.Windows.Forms.Label();
+            this.lblT1614 = new System.Windows.Forms.Label();
+            this.btn1518 = new System.Windows.Forms.Button();
+            this.btnT1614 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblT1518
+            // 
+            this.lblT1518.AutoSize = true;
+            this.lblT1518.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT1518.Location = new System.Drawing.Point(155, 164);
+            this.lblT1518.Name = "lblT1518";
+            this.lblT1518.Size = new System.Drawing.Size(266, 22);
+            this.lblT1518.TabIndex = 0;
+            this.lblT1518.Text = "T1518 System Discovery Attack";
+            // 
+            // lblT1614
+            // 
+            this.lblT1614.AutoSize = true;
+            this.lblT1614.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblT1614.Location = new System.Drawing.Point(598, 164);
+            this.lblT1614.Name = "lblT1614";
+            this.lblT1614.Size = new System.Drawing.Size(341, 22);
+            this.lblT1614.TabIndex = 1;
+            this.lblT1614.Text = "T1614 System Location Discovery Attack";
+            // 
+            // btn1518
+            // 
+            this.btn1518.BackColor = System.Drawing.Color.Red;
+            this.btn1518.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1518.Location = new System.Drawing.Point(183, 253);
+            this.btn1518.Name = "btn1518";
+            this.btn1518.Size = new System.Drawing.Size(192, 78);
+            this.btn1518.TabIndex = 2;
+            this.btn1518.Text = "T1518";
+            this.btn1518.UseVisualStyleBackColor = false;
+            this.btn1518.Click += new System.EventHandler(this.btn1518_Click);
+            // 
+            // btnT1614
+            // 
+            this.btnT1614.BackColor = System.Drawing.Color.Red;
+            this.btnT1614.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnT1614.Location = new System.Drawing.Point(667, 253);
+            this.btnT1614.Name = "btnT1614";
+            this.btnT1614.Size = new System.Drawing.Size(192, 78);
+            this.btnT1614.TabIndex = 3;
+            this.btnT1614.Text = "T1614";
+            this.btnT1614.UseVisualStyleBackColor = false;
+            this.btnT1614.Click += new System.EventHandler(this.btnT1614_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 672);
+            this.Controls.Add(this.btnT1614);
+            this.Controls.Add(this.btn1518);
+            this.Controls.Add(this.lblT1614);
+            this.Controls.Add(this.lblT1518);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblT1518;
+        private System.Windows.Forms.Label lblT1614;
+        private System.Windows.Forms.Button btn1518;
+        private System.Windows.Forms.Button btnT1614;
+    }
+}
+
---
+++ MitreAttack/MitreAttack/Test.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MitreAttack
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1518_Click(object sender, EventArgs e)
+        {
+            SaveAttackDatabase("T1518");
+        }
+
+        private void btnT1614_Click(object sender, EventArgs e)
+        {
+            SaveAttackDatabase("T1614");
+        }
+
+        public void SaveAttackDatabase(string attackCode)
+        {
+            string query = "";
+            if (attackCode == "T1518")
+            {
+                query = "reg query \"HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Internet Explorer\" /v svcVersion";
+            }
+            else if (attackCode == "T1614")
+            {
+                query = "reg query \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Nls\\Language\"";
+            }
+            else
+            {
+                MessageBox.Show("Geçersiz attack ID.");
+                return;
+            }
+
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\System32\cmd.exe",
+                Arguments = $"/c {query}",
+                Verb = "runas", // CMD'yi yönetici olarak çalıştır
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo = processInfo;
+                    process.Start();
+                    string result = process.StandardOutput.ReadToEnd(); // CMD çıktısını oku
+                    process.WaitForExit();
+
+                    string connectionString = "Data Source=**IP**\\SQLEXPRESS;Initial Catalog=MitreAttackDB;User Id=MITRE;Password=1234;";
+
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string sql = "INSERT INTO ATTACKS (AttackResult, AttackCode) VALUES (@AttackResult, @AttackCode)";
+                        SqlCommand command = new SqlCommand(sql, connection);
+                        command.Parameters.AddWithValue("@AttackResult", result);
+                        command.Parameters.AddWithValue("@AttackCode", attackCode);
+
+                        try
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Veritabanına kayıt yapılamadı: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (Win32Exception)
+            {
+                MessageBox.Show("Komut yönetici olarak çalıştırılamadı.");
+            }
+        }
+    }
+}
